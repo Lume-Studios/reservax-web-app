40ec6767b9743e462835f3be36ab9a2416ad0bd9
Nicolau Riess <nicolauriess@gmail.com>
2022-04-04 18:22:33 -0300

    build name changed for cdn

--- webpack.common.js
+++ webpack.common.js
@@ -15,7 +15,7 @@ module.exports = {
         extensions: ['.tsx', '.ts', '.js']
     },
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle_v1.js',
         path: path.resolve(__dirname, 'dist')
     }
 };
